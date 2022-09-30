8c60c3b32c00ce987054a8bbf4ac1dc5164d11fc
lokesh1306 <challagulla.l@northeastern.edu>
2022-09-30 18:44:23 -0400

    Source Packages

---
+++ Assignment_1/src/Employee/Profile.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Employee;
+
+/**
+ *
+ * @author admin
+ */
+public class Profile {
+    
+    private String Name;
+    private int EmployeeID;
+    private int Age;
+    private String Gender;
+    private String StartDate;
+    private String Level;
+    private String TeamInfo;
+    private String PositionTitle;
+    private long PhoneNumber;
+    private String EmailAddress;
+    private String Photo;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getEmployeeID() {
+        return EmployeeID;
+    }
+
+    public void setEmployeeID(int EmployeeID) {
+        this.EmployeeID = EmployeeID;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public String getStartDate() {
+        return StartDate;
+    }
+
+    public void setStartDate(String StartDate) {
+        this.StartDate = StartDate;
+    }
+
+    public String getLevel() {
+        return Level;
+    }
+
+    public void setLevel(String Level) {
+        this.Level = Level;
+    }
+
+    public String getTeamInfo() {
+        return TeamInfo;
+    }
+
+    public void setTeamInfo(String TeamInfo) {
+        this.TeamInfo = TeamInfo;
+    }
+
+    public String getPositionTitle() {
+        return PositionTitle;
+    }
+
+    public void setPositionTitle(String PositionTitle) {
+        this.PositionTitle = PositionTitle;
+    }
+
+    public long getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(long PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+
+    public String getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(String Photo) {
+        this.Photo = Photo;
+    }
+    
+}
---
+++ Assignment_1/src/Employee/ProfileHistory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Employee;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class ProfileHistory {
+    
+    private ArrayList<Profile> history;
+    
+    public ProfileHistory(){
+        
+        this.history = new ArrayList<Profile>();
+    }
+
+    public ArrayList<Profile> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<Profile> history) {
+        this.history = history;
+    }
+    
+}
